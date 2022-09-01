48214bbba73311b346898b871559750999068bbb
shriramrathod470 <shriramrathod47@gmail.com>
2022-09-01 18:30:18 +0530

    edited

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export const NotFound = () => {
     return (
-       
+        <>
+        <div id = "not-found-page">
+        Not found page
+        </div>
+        </>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,15 @@ import React from 'react';
 
 export const NavBar = () =>{
     return(
+        <>
+        <div>
+            <NavLink path = "/" class ="index-link">
 
+            </NavLink>
+            <NavLink  path = "/home" class = "home-link">
+
+            </NavLink>
+        </div>
+        </>
     )
 }
